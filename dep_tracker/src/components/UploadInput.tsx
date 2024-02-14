4fdebb4b6a6e2a60a44c65c56e492db009e920ff
mcubas <86804637+matheuscubas@users.noreply.github.com>
2024-02-13 21:48:36 -0300

    ✨ Finish upload input logic

--- dep_tracker/src/components/UploadInput.tsx
+++ dep_tracker/src/components/UploadInput.tsx
@@ -1,4 +1,5 @@
 import { useField } from "formik";
+import { ChangeEvent } from "react";
 import { MdError } from "react-icons/md";
 
 interface UploadInputProps {
@@ -8,8 +9,18 @@ interface UploadInputProps {
 }
 
 export function UploadInput({ label, name, description }: UploadInputProps) {
-  const [field, meta] = useField(name);
+  const [field, meta, helpers] = useField(name);
   const hasError = meta.touched && meta.error;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { value, ...rest } = field;
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+
+    const file = Array.from(e.target.files)[0];
+    console.log(file);
+
+    helpers.setValue(file);
+  };
 
   return (
     <div className="max-w-lg mx-auto">
@@ -23,10 +34,11 @@ export function UploadInput({ label, name, description }: UploadInputProps) {
         className={`block w-full text-sm text-gray-900 border ${
           hasError ? "border-red-500 border-2" : "border-gray-300"
         } rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400`}
-        {...field}
+        {...rest}
         aria-describedby={`${name}_help`}
         id={name}
         type="file"
+        onChange={handleChange}
       />
       {hasError ? (
         <p className="text-sm text-red-900 text-left flex font-bold">
