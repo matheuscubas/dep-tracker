62c5623f0f8e7b661dbbc3068aa69d1b68ff759d
mcubas <86804637+matheuscubas@users.noreply.github.com>
2024-10-22 23:39:45 -0300

    🔥 Remove commented code

--- dep_tracker/src/components/Gallery.tsx
+++ dep_tracker/src/components/Gallery.tsx
@@ -31,14 +31,10 @@ export default function Gallery({galleryData}: GalleryProps) {
       </Card>
     )
   })
-  return (
 
+  return (
     <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
       {cards}
-      {/*<div>*/}
-      {/*  <img className="h-auto max-w-full rounded-lg"*/}
-      {/*       src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt=""/>*/}
-      {/*</div>*/}
     </div>
   )
 }
