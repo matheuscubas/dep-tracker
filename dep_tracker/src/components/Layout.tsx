78e45296e70d2c7005cf14ef4fc27646210db694
mcubas <86804637+matheuscubas@users.noreply.github.com>
2024-10-22 23:38:16 -0300

    🐛 Fix display tittle not centered on small screens

--- dep_tracker/src/components/Layout.tsx
+++ dep_tracker/src/components/Layout.tsx
@@ -4,15 +4,15 @@ export default function Layout({children}: PropsWithChildren) {
   return (
     <div className="container m-0 p-0 w-screen h-screen text-gray-300 font-sans flex flex-col">
       {/* header */}
-      <div className="background-color: bg-green-700 w-screen h-32 flex justify-center pb-24">
+      <div className="background-color: bg-green-700 w-screen h-32 flex justify-center pb-24 text-center">
         <div>
-          <h1 className="pl-4 mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+          <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
             <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
               Dep
             </span>
             Tracker™
           </h1>
-          <p className="text-center text-gray-900 font-extrabold">
+          <p className="text-gray-900 font-extrabold">
             quickly identify all upgradable dependencies from your project!
           </p>
         </div>
